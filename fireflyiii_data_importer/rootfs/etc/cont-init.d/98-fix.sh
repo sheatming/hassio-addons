4829043185bf17eda0f0ed6346eee6dc3a64c2e7
sheatming <sheatming@hotmail.com>
2023-08-18 09:30:36 +0800

    Delete fireflyiii_data_importer directory

--- fireflyiii_data_importer/rootfs/etc/cont-init.d/98-fix.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Fix for Nordigen"
-sed -i $"s/'description'.*/\'description\'        => \'\' !== trim((string)(\$entry->getDescription() ?? \'\')) ? \$entry->getDescription() : \'(no description)\',/" /var/www/html/app/Services/Nordigen/Conversion/Routine/GenerateTransactions.php
